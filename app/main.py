c194c81945839d6df4b6172c70d0ce52c263070d
Egemen Çelik <egemen.celik@alpata.com>
2024-10-27 18:26:14 +0300

    added mock authentication

--- app/main.py
+++ app/main.py
@@ -1,9 +1,32 @@
 from contextlib import asynccontextmanager
-from typing import Annotated, Optional
-from fastapi import Depends, FastAPI
+from typing import Annotated, Optional, Union
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from sqlalchemy import create_engine
 from sqlmodel import SQLModel, Session
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
+
+
+def fake_hash_password(password: str):
+    return "fakehashed" + password
+
 
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -15,11 +38,14 @@ engine = create_engine(sqlite_url, connect_args=connect_args)
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
+
 def get_session():
     with Session(engine) as session:
         yield session
 
+
 SessionDep = Annotated[Session, Depends(get_session)]
+
 
 class Item(BaseModel):
     name: str
@@ -28,25 +54,101 @@ class Item(BaseModel):
     tax: Optional[float] = None
 
 
+class User(BaseModel):
+    username: str
+    email: Union[str, None] = None
+    full_name: Union[str, None] = None
+    disabled: Union[bool, None] = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Uygulama başlatıldığında çalışacak kod
     create_db_and_tables()
-    yield 
-    
+    yield
+
 app = FastAPI(lifespan=lifespan)
-    
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def fake_decode_token(token):
+    # This doesn't provide any security at all
+    # Check the next version
+    user = get_user(fake_users_db, token)
+    return user
+
+#  The oauth2_scheme dependency automatically extracts the token from the 
+#  Authorization header in the request
+# Authorization: Bearer <token>
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    print(f"Token received: {token}")  # Console log
+    user = fake_decode_token(token)
+    print(f"User decoded from token: {user}")  # Console log
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/users/me")
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return current_user
+
+
+@app.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(
+            status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(
+            status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": form_data.username, "token_type": "bearer"}
+
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
 
+
+@app.get("/items/")
+async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
+    return {"token": token}
+
+
 @app.post("/items/")
 async def create_item(item: Item):
     return item.name.capitalize()
-
-
-
--- app/models/base.py
+++ app/models/base.py
@@ -3,10 +3,10 @@ from datetime import datetime
 from typing import Optional
 
 class BaseModel(SQLModel):
-    id: int | None = Field(default=None, primary_key=True)
+    id: Optional[int] = Field(default=None, primary_key=True)
     created_at: datetime = Field(default_factory=datetime.time(datetime.timezone.utc))
     updated_at: Optional[datetime] = Field(default=None)
-    
+        
     # Method to update the timestamp automatically
     def update_timestamp(self):
         self.updated_at = datetime.now(datetime.timezone.utc)
