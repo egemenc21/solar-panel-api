3aa144980131e3815ae5d547aaebb08d149ac726
Egemen Çelik <egemen.celik@alpata.com>
2024-10-27 16:19:53 +0300

    testing the api and following docs

--- app/main.py
+++ app/main.py
@@ -1,8 +1,52 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from typing import Annotated, Optional
+from fastapi import Depends, FastAPI
+from pydantic import BaseModel
+from sqlalchemy import create_engine
+from sqlmodel import SQLModel, Session
 
-app = FastAPI()
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
 
 
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Uygulama başlatıldığında çalışacak kod
+    create_db_and_tables()
+    yield 
+    
+app = FastAPI(lifespan=lifespan)
+    
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item.name.capitalize()
+
+
+
