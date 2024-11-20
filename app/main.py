6fce776f293972f9699404230745372d7353786f
egemenc <egemenc2101@gmail.com>
2024-11-20 23:46:42 +0300

    Fix CORS middleware configuration by commenting out unused origins

--- app/main.py
+++ app/main.py
@@ -78,7 +78,7 @@ app = FastAPI(lifespan=lifespan)
 #     "https://localhost.tiangolo.com",
 #     "http://localhost",
 #     "http://localhost:8080",
-]
+# ]
 
 app.add_middleware(
     CORSMiddleware,
